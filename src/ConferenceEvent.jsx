c055d3296a64f5a1f2cbf3e09aebaeba10f01401
Destin B.Moanda <moanda@live.fr>
2026-02-07 16:53:09 -0500

    add addsons

--- src/ConferenceEvent.jsx
+++ src/ConferenceEvent.jsx
@@ -8,7 +8,7 @@ const ConferenceEvent = () => {
     const [numberOfPeople, setNumberOfPeople] = useState(1);
     const venueItems = useSelector((state) => state.venue);
     const avItems=useSelector((state)=>state.av);
-    
+
     const dispatch = useDispatch();
     const remainingAuditoriumQuantity = 3 - venueItems.find(item => item.name === "Auditorium Hall (Capacity:200)").quantity;
 
@@ -150,6 +150,21 @@ const ConferenceEvent = () => {
       </div>
 
                             {/*Necessary Add-ons*/}
+                            {avItems.map((item, index) => (
+                                <div className="av_data venue_main" key={index}>
+                                    <div className="img">
+                                        <img src={item.img} alt={item.name} />
+                                    </div>
+                                <div className="text"> {item.name} </div>
+                                <div> ${item.cost} </div>
+                                    <div className="addons_btn">
+                                        <button className="btn-warning" onClick={() => handleDecrementAvQuantity(index)}> &ndash; </button>
+                                        <span className="quantity-value">{item.quantity}</span>
+                                        <button className=" btn-success" onClick={() => handleIncrementAvQuantity(index)}> &#43; </button>
+                                    </div>
+                                </div>
+                            ))}
+
                             <div id="addons" className="venue_container container_main">
 
 
